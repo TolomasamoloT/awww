33b7b71d0bc702e6e5fcd8d5f8c199c3f6e85644
Anatol Navicki <anatol.navicki@icloud.com>
2025-04-15 15:33:26 +0200

    test

--- anime_on_scroll/app.js
+++ anime_on_scroll/app.js
@@ -7,3 +7,5 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// owo
